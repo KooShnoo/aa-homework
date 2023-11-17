a584c1a94f6d7d433998745cf4aaea369b825891
Ishan Chawla <ishan.kpv@gmail.com>
2023-11-16 23:09:02 -0500

    init hw movie buff traingin

--- hw_w5/practice-for-ch-sql-intro-to-rails-main/housing-devlopment/app/models/house.rb
+++ hw_w5/practice-for-ch-sql-intro-to-rails-main/housing-devlopment/app/models/house.rb
@@ -1,7 +1,17 @@
+# == Schema Information
+#
+# Table name: houses
+#
+#  id          :bigint           not null, primary key
+#  address     :string
+#  resident_id :integer
+#  created_at  :datetime         not null
+#  updated_at  :datetime         not null
+#
 class House < ApplicationRecord
   validates :address, presence: true
   has_many(
     :residents,
-    class_name: Person.to_s,
+    class_name: Person.to_s
   )
 end
--- hw_w5/practice-for-ch-sql-intro-to-rails-main/housing-devlopment/app/models/person.rb
+++ hw_w5/practice-for-ch-sql-intro-to-rails-main/housing-devlopment/app/models/person.rb
@@ -1,3 +1,17 @@
+# == Schema Information
+#
+# Table name: people
+#
+#  id         :bigint           not null, primary key
+#  name       :string
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#  house_id   :bigint
+#
+# Indexes
+#
+#  index_people_on_house_id  (house_id)
+#
 class Person < ApplicationRecord
   validates :name, :house, presence: true
   belongs_to :house
